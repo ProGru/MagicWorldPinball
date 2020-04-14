b7eadc4360f9dc295ee520766c3fb8a1b9543f39
ProGru <adamowskijad22@op.pl>
2020-04-14 17:54:32 +0200

    Init Commit

---
+++ MagicWorldPinball/Assets/Scripts/EndGame.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EndGame : MonoBehaviour
+{
+    public GameObject spawnPoint;
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        collision.gameObject.transform.position = spawnPoint.transform.position;
+    }
+}
---
+++ MagicWorldPinball/Assets/Scripts/FlipperControler.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlipperControler : MonoBehaviour
+{
+    public HingeJoint2D lefthingeJoint2D;
+    public HingeJoint2D righthingeJoint2D;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+
+    IEnumerator FlipperRight()
+    {
+        righthingeJoint2D.useMotor = true;
+        //yield on a new YieldInstruction that waits for 5 seconds.
+        yield return new WaitForSeconds(0.1f);
+
+        righthingeJoint2D.useMotor = false;
+    }
+
+    IEnumerator FlipperLeft()
+    {
+        lefthingeJoint2D.useMotor = true;
+        //yield on a new YieldInstruction that waits for 5 seconds.
+        yield return new WaitForSeconds(0.1f);
+
+        lefthingeJoint2D.useMotor = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            StartCoroutine(FlipperLeft());
+        }
+
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            StartCoroutine(FlipperRight());
+        }
+
+    }
+}
